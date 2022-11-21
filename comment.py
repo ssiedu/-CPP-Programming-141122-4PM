392d0a12ab912b4c91be410c14c077595f8edda8
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-21 19:58:23 +0530

    program

---
+++ comment.py
@@ -0,0 +1,15 @@
+#This is a single line comment
+print("Single Line comments ")
+"""This is
+a Multiline
+Comment"""
+print("Multiline comment")
+
+def Hello():
+    '''This is a docstring Comment'''
+    print("This is a function ")
+
+
+
+
+print(Hello.__doc__)
