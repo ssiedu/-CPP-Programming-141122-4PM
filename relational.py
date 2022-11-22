4262a8e4eec81de365975aa6b1f2d180d60dcbdc
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-22 20:00:36 +0530

    program

---
+++ relational.py
@@ -0,0 +1,8 @@
+a=int(input("Enter First Value : "))#50
+b=int(input("Enter Second Value : "))#100
+print("a>b : ", a>b)#False
+print("a<b : ", a<b)#True
+print("a>=b :",a>=b)#False
+print("a<=b : ", a<=b)#True
+print("a==b : ", a==b)#False
+print("a!=b : ", a!=b)#True
