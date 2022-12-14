aa0947a87927b98793cd101c8da6c2cc5e6edbcb
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-12-14 19:53:12 +0530

    program

---
+++ fun1.py
@@ -0,0 +1,8 @@
+def Hello():
+    print("Hello Everyone")
+    print("Welcome to ssi")
+    print("This is python class")
+
+Hello()
+Hello()
+Hello()
---
+++ funadd.py
@@ -0,0 +1,7 @@
+def add():
+    a=int(input("Enter the value of a : "))
+    b=int(input("Enter the value of b : "))
+    print("sum is : ", a+b)
+
+add()
+add()
